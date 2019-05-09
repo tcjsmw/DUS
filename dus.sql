d9e3538ed848ca6aaf2e17a12430b50d0d281ab7
ZiyiShen <50234801+ZiyiShen@users.noreply.github.com>
2019-05-09 21:05:03 +0100

    Database
    I built it for testing. Can be imported to your localhost if needed.

---
+++ dus.sql
@@ -0,0 +1,195 @@
+-- MySQL dump 10.13  Distrib 5.6.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: dus
+-- ------------------------------------------------------
+-- Server version	5.7.20-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `booking`
+--
+
+DROP TABLE IF EXISTS `booking`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `booking` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `is_fixed` int(11) DEFAULT NULL,
+  `title` text,
+  `content` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `booking`
+--
+
+LOCK TABLES `booking` WRITE;
+/*!40000 ALTER TABLE `booking` DISABLE KEYS */;
+INSERT INTO `booking` VALUES (1,0,NULL,NULL),(2,1,'textTitle','textContent');
+/*!40000 ALTER TABLE `booking` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `booking_facility`
+--
+
+DROP TABLE IF EXISTS `booking_facility`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `booking_facility` (
+  `booking_id` int(11) NOT NULL,
+  `facility_id` int(11) NOT NULL,
+  PRIMARY KEY (`booking_id`,`facility_id`),
+  KEY `facility_id` (`facility_id`),
+  CONSTRAINT `booking_facility_ibfk_1` FOREIGN KEY (`booking_id`) REFERENCES `booking` (`id`),
+  CONSTRAINT `booking_facility_ibfk_2` FOREIGN KEY (`facility_id`) REFERENCES `facility` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `booking_facility`
+--
+
+LOCK TABLES `booking_facility` WRITE;
+/*!40000 ALTER TABLE `booking_facility` DISABLE KEYS */;
+INSERT INTO `booking_facility` VALUES (1,1),(2,1);
+/*!40000 ALTER TABLE `booking_facility` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `booking_timeslot`
+--
+
+DROP TABLE IF EXISTS `booking_timeslot`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `booking_timeslot` (
+  `booking_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `slot` int(11) NOT NULL,
+  PRIMARY KEY (`booking_id`,`date`,`slot`),
+  CONSTRAINT `booking_timeslot_ibfk_1` FOREIGN KEY (`booking_id`) REFERENCES `booking` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `booking_timeslot`
+--
+
+LOCK TABLES `booking_timeslot` WRITE;
+/*!40000 ALTER TABLE `booking_timeslot` DISABLE KEYS */;
+INSERT INTO `booking_timeslot` VALUES (1,'2019-05-09',13),(2,'2019-05-09',17),(2,'2019-05-10',17);
+/*!40000 ALTER TABLE `booking_timeslot` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `facility`
+--
+
+DROP TABLE IF EXISTS `facility`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `facility` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `description` text,
+  `price` int(11) DEFAULT NULL,
+  `capacity` int(11) DEFAULT NULL,
+  `contact_email` text,
+  `contact_tel` text,
+  `contact_address` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `facility`
+--
+
+LOCK TABLES `facility` WRITE;
+/*!40000 ALTER TABLE `facility` DISABLE KEYS */;
+INSERT INTO `facility` VALUES (1,'Athletic Track',NULL,NULL,20,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `facility` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `password` text NOT NULL,
+  `email` text NOT NULL,
+  `gender` varchar(10) DEFAULT NULL,
+  `pic` text,
+  `is_manager` int(11) NOT NULL,
+  `is_uni_member` int(11) DEFAULT NULL,
+  `security_question` text,
+  `security_answer` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'testname','testpsw','testem',NULL,NULL,1,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_booking`
+--
+
+DROP TABLE IF EXISTS `user_booking`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_booking` (
+  `booking_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`booking_id`,`user_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `user_booking_ibfk_1` FOREIGN KEY (`booking_id`) REFERENCES `booking` (`id`),
+  CONSTRAINT `user_booking_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_booking`
+--
+
+LOCK TABLES `user_booking` WRITE;
+/*!40000 ALTER TABLE `user_booking` DISABLE KEYS */;
+INSERT INTO `user_booking` VALUES (1,1);
+/*!40000 ALTER TABLE `user_booking` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-05-09 21:00:52
